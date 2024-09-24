f8e859a3f6152682e8379887651eff3e37a8d1d9
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2024-09-24 16:34:40 +0530

    chat

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -119,9 +119,9 @@ const handleButtonClick = () => {
     </button>
     {isCommentBoxVisible && (
         <div className="comment-box">
-            <form className="comment-submit">
+            <form className="comment-submit w-60">
                 <label for="chat" class="sr-only">Your Comment</label>
-                <div class="flex items-center p-3 rounded-lg bg-gray-50 dark:bg-gray-700">
+                <div class="flex items-center p-2 rounded-lg bg-gray-50 dark:bg-gray-700">
                     <textarea id="chat" rows="3" class="block p-2 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
                     <button type="submit" class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
                         <svg class="w-5 h-5" aria-hidden="true" fill="#44b8f3" viewBox="0 0 448 448">
